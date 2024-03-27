6551aa991bc4d6db7867fd6c5b2420ea509f2e23
Kundavarampradeep <kundavarampradeep8@gmail.com>
2024-03-27 15:31:10 +0530

    script added

--- variables.sh
+++ variables.sh
@@ -12,7 +12,8 @@ P2=Rohit
 echo "$P1 : Hi $P2 , Good Morning"
 echo "$P2 : Hi $P1 , Good Morning"
 
-#shell script run the command inside $() and stores the output into variable called DATE.
+#!/bin/bash
+eecho "shell script run the command inside $() and stores the output into variable called DATE."
 DATE=$(date)
 
 echo "This script is executed at timestamp: $DATE

9b86bbc977888a6c383e674aa07d082ad47c40d9
Kundavarampradeep <kundavarampradeep8@gmail.com>
2024-03-29 18:06:21 +0530

    special variables

--- special-variables.sh
+++ special-variables.sh
@@ -5,7 +5,7 @@ VAR=$2
 
 echo " script name :- $0 "  #it gives sccript name
 
-echo " First variable is $VAR1 "  #while executing if you give 20 and 21 it will give only 20
+echo "Variable 1 : $VAR1"  #while executing if you give 20 and 21 it will give only 20
  #echo " Second variable is $VAR2 "  #it will give 21
 
  echo " all variables $@ " #if you give numbers it will give 20 and 21

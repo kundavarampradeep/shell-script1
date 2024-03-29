f720281680e72c426dd19894756deac870d3e04c
Kundavarampradeep <kundavarampradeep8@gmail.com>
2024-03-29 15:49:23 +0530

    with condition

--- condition.sh
+++ condition.sh
@@ -5,19 +5,16 @@ USERID=$(id -u)
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-    echo " $1 Insatallation .... FAILURE"
+    echo " $2 Insatallation .... FAILURE"
     else
-    echo " $1 Installation .... SUCCESS"
+    echo " $2 Installation .... SUCCESS"
     fi
 }
 
-if [ $USERID -ne 0 ] #thia means user id is not equal to zero
+if [ $USERID -ne 0 ]
 then
 echo "ERROR :: Please run this with root user"
-exit 1  # shell or linux will under stand it is error so it will stop till here, linux error is 1 to 127 if it is 0 it's success
-# if we don't use exit commamnd linix will execute below script that is not correct way to follow rules in programming.
-# else
-# echo "INFO:: You are root user"
+exit 1  
 fi
     yum install mysql -y
     VALIDATE $? " mysql installation "

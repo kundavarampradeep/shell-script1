7353a4940707fa01c67c4678c747315ebff577da
Kundavarampradeep <kundavarampradeep8@gmail.com>
2024-03-29 16:09:23 +0530

    with condition

--- conditions.sh
+++ conditions.sh
@@ -2,26 +2,27 @@
 
 USERID=$(id -u)
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-    echo " $2 .... FAILURE"
-    else
-    echo " $2 .... SUCCESS"
-    fi
-}
-
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ] #thia means user id is not equal to zero
 then
 echo "ERROR :: Please run this with root user"
-exit 1  
+#exit 1  # shell or linux will under stand it is error so it will stop till here, linux error is 1 to 127 if it is 0 it's success
+# else
+# echo "INFO:: You are root user"
 fi
     yum install mysql -y
-    VALIDATE $? " mysql installation "
+if [ $? -ne 0 ]    
+then
+echo " mysql installation failed"
+else
+echo "mysql installation is successfull"
+fi
+    #here the out put is only error, because of exit below script won't execute.
 
+  
     yum install java -y
-    VALIDATE $? " java installation"
-
-    yum install git -y
-    VALIDATE $? " git installation"
-    
+    if [ $? -ne 0 ]    
+then
+echo " java installation failed"
+else
+echo "java installation is successfull"
+fi

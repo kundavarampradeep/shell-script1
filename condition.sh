083287d46d5f4a4b39354051886e8400f104fa33
Kundavarampradeep <kundavarampradeep8@gmail.com>
2024-03-29 10:17:59 +0530

    with condition

--- condition.sh
+++ condition.sh
@@ -2,8 +2,11 @@
 
 NUMBER=$1
 
-if [ $NUMBER -gt 10 ]
+if [ $NUMBER -gt 10 ] #this means if number is greater than 10 
 then
 echo "$NUMBER is greater than 10"
+else
+echo " $NUMBER is not greater than 10"
 
 fi
+# while executing we have to pass one number

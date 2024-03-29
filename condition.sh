e24c4991a2ef21560924e6e72943db9b762ec70c
Kundavarampradeep <kundavarampradeep8@gmail.com>
2024-03-29 11:01:03 +0530

    with condition

--- condition.sh
+++ condition.sh
@@ -2,11 +2,12 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ] #thia means user id is not equal to zero
 then
 echo "ERROR :: Please run this with root user"
-exit 1
-else
-echo "INFO:: You are root user"
+exit 1  # shell or linux will under stand it is error so it will stop till here, linux error is 1 to 127 if it is zero success
+# else
+# echo "INFO:: You are root user"
 fi
     yum install mysql -y
+    #here the out put is only error, because of exit below script won't execute.

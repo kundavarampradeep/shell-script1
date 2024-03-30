40505e01de96865fe9bd35b8800deec932d9f150
Kundavarampradeep <kundavarampradeep8@gmail.com>
2024-03-30 11:14:42 +0530

    loops condition

---
+++ loops.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+Y="\e[33m"
+N="\e[0m"
+
+for i in $@
+do
+yum install $i -y &>>$LOGFILE
+done
+
+VALIDATE=(){
+    if [ $1 -ne 0 ]
+    then
+    echo -e " $2... $R FAILURE $N "
+    else
+    echo -e " $2... $Y SUCCESS $N "
+fi
+}
+ 
+ USERID=$(id -u)
+ 
+ if [ $USERID -ne 0 ]
+ then
+ echo " ERROR :: Please run this with root user "
+ exit1
+ fi

1cae256d9a1ecc14451f4a31774ec99be5d88ad5
Kundavarampradeep <kundavarampradeep8@gmail.com>
2024-03-30 18:06:25 +0530

    loops condition

--- loops.sh
+++ loops.sh
@@ -3,7 +3,7 @@
 DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
-
+USERID=$(id -u)
 R="\e[31m"
 Y="\e[33m"
 N="\e[0m"
@@ -12,20 +12,29 @@ VALIDATE=(){
     if [ $1 -ne 0 ]
     then
     echo -e " $2... $R FAILURE $N "
+    exit1
     else
     echo -e " $2... $Y SUCCESS $N "
     fi
 }
  
- USERID=$(id -u)
+
  
  if [ $USERID -ne 0 ]
  then
- echo " ERROR :: Please run this with root user "
+ echo -e " ERROR :: Please run this with root user $N "
  exit1
  fi
 
  for i in $@
 do
-yum install $i -y &>>$LOGFILE
+ yum install $i &>>$LOGFILE
+ if [ $?-ne 0 ]
+ then
+ echo " $i is not installed let's install it "
+ yum install $i -y &>>$LOGFILE
+ VALIDATE $? "$i"
+ else
+ echo -e " $Y $i is already installed $N "
+ fi
 done

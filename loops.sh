cd89037cf8e15572b312c164c605a51643b73a78
Kundavarampradeep <kundavarampradeep8@gmail.com>
2024-03-30 18:11:11 +0530

    loops condition

--- loops.sh
+++ loops.sh
@@ -8,18 +8,15 @@ R="\e[31m"
 Y="\e[33m"
 N="\e[0m"
 
-VALIDATE=(){
-    if [ $1 -ne 0 ]
+VALIDATE(){
+    if [ $1 -ne 0 ];
     then
-    echo -e " $2... $R FAILURE $N "
-    exit1
+        echo -e "Installing $2 ... $R FAILURE $N"
+        exit 1
     else
-    echo -e " $2... $Y SUCCESS $N "
+        echo -e "Installing $2 ... $G SUCCESS $N"
     fi
 }
- 
-
- 
  if [ $USERID -ne 0 ]
  then
  echo -e " ERROR :: Please run this with root user $N "

3daf0246cfb4e53f9c5ae7ee7c427640cca32f70
Kundavarampradeep <kundavarampradeep8@gmail.com>
2024-04-05 21:27:57 +0530

    delete.oldlogs

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -5,6 +5,6 @@ LOGSDIR=/home/centos/shell-script1-logs
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$0-$DATE.log
 
-FILES_TO_DELETE=$(find $APP_LOG_DIR -name "*.log" -type f +mtime +14)
+FILES_TO_DELETE=$(find $APP_LOG_DIR -name "*.log" -type f -mtime +14)
 
 echo "$FILES_TO_DELETE deleted"

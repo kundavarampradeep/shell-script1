f999f0d560917fdf811d945b61799bb2b81863d9
Kundavarampradeep <kundavarampradeep8@gmail.com>
2024-04-08 15:23:48 +0530

    delete.oldlogs

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -1,5 +1,5 @@
 
-APP_LOG_DIR=/tmp/app-logs
+APP_LOG_DIR=/home/centos/app-logs
 DATE=$(date +%F)
 LOGSDIR=/home/centos/shellscript-logs
 SCRIPT_NAME=$0
@@ -8,3 +8,9 @@ LOGFILE=$LOGSDIR/$0-$DATE.log
 FILES_TO_DELETE=$(find $APP_LOG_DIR -name "*.log" -type f -mtime +14)
 
 echo "$FILES_TO_DELETE "
+
+while read line
+do
+echo "Deleting $line " &> $LOGFILE
+rm -rf $line
+done <<< $FILES_TO_DELETE

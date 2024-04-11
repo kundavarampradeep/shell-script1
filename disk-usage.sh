7912acae2517f736ce63ecd8b6a1fbc677a97cf1
Kundavarampradeep <kundavarampradeep8@gmail.com>
2024-04-11 11:22:20 +0530

    script for diskusage

---
+++ disk-usage.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#colors
+#validations
+#redirections
+
+LOGFILE_DIRECTORY=/tmp
+DATE=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$0
+LOGFILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
+DISK_USAGE_THRESHOLD=1
+message=""
+
+#IFS= means internal feild seperator is space.
+while IFS= read line
+do
+    usage=$(echo $line | awk '{print$6}' | cut -d % -f1)   # this command will give you usage in number format for comparision
+    partition=$(echo $line | awk '{print $1}')    # this command will give us partition
+if [ $usage -gt $DISK_USAGE_THRESHOLD ] ;
+then
+    message+=" HIGH DISK USAGE on $partition : $usage\n "
+fi
+done <<< $DISK_USAGE
+echo -e "message: $message"
